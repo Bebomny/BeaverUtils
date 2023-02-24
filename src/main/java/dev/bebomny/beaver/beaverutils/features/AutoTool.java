c67f58244ea8c304e1e0987b86139eb7513cd41d
Domniakk <lukaskrzyszewski@gmail.com>
2023-02-24 17:34:28 +0100

    AutoTool Added

---
+++ src/main/java/dev/bebomny/beaver/beaverutils/features/AutoTool.java
@@ -0,0 +1,94 @@
+package dev.bebomny.beaver.beaverutils.features;
+
+import dev.bebomny.beaver.beaverutils.client.BeaverUtilsClient;
+import dev.bebomny.beaver.beaverutils.helpers.Notification;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import java.awt.*;
+
+public class AutoTool extends Feature{
+    private final MinecraftClient client;
+    private final BeaverUtilsClient modBeaverUtils;
+
+    public AutoTool(MinecraftClient client, BeaverUtilsClient mod) {
+        super("AutoTool");
+        this.client = client;
+        this.modBeaverUtils = mod;
+    }
+
+    public void onBlockBreakingEvent(BlockPos pos, Direction direction) {
+        if(!isEnabled())
+            return;
+
+        equipBestTool(pos);
+
+    }
+
+    private void equipBestTool(BlockPos pos) {
+        ClientPlayerEntity player = client.player;
+        if(player.getAbilities().creativeMode)
+            return;
+
+        int bestSlot = getBestSlot(pos);
+        if(bestSlot == -1)
+            return;
+
+        player.getInventory().selectedSlot = bestSlot;
+    }
+
+    private int getBestSlot(BlockPos pos) {
+        ClientPlayerEntity player = client.player;
+        PlayerInventory playerInventory = player.getInventory();
+        ItemStack heldItem = client.player.getMainHandStack();
+
+        BlockState state = client.world.getBlockState(pos);
+        float bestSpeed = getMiningSpeed(heldItem, state);
+        int bestSlot = -1;
+
+        for(int slot = 0; slot < 9; slot++) {
+            if(slot == playerInventory.selectedSlot)
+                continue;
+
+            ItemStack stack = playerInventory.getStack(slot);
+
+            float speed = getMiningSpeed(stack, state);
+            if(speed <= bestSpeed)
+                continue;
+
+            bestSpeed = speed;
+            bestSlot = slot;
+        }
+
+        return bestSlot;
+    }
+
+    private float getMiningSpeed(ItemStack stack, BlockState state) {
+        float speed = stack.getMiningSpeedMultiplier(state);
+
+        if(speed > 1) {
+            int efficiency = EnchantmentHelper.getLevel(Enchantments.EFFICIENCY, stack);
+
+            if(efficiency > 0 && !stack.isEmpty())
+                speed += efficiency * efficiency + 1;
+
+        }
+
+        return speed;
+    }
+
+    protected void onEnable() {
+        modBeaverUtils.notifier.newNotification(new Notification(Text.literal("AutoTool Enabled"), new Color(0x00FF00)));
+    }
+
+    protected void onDisable() {
+        modBeaverUtils.notifier.newNotification(new Notification(Text.literal("AutoTool Disabled"), new Color(0xFF00000)));
+    }
+}
--- src/main/java/dev/bebomny/beaver/beaverutils/mixins/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/dev/bebomny/beaver/beaverutils/mixins/ClientPlayerInteractionManagerMixin.java
@@ -2,6 +2,8 @@ package dev.bebomny.beaver.beaverutils.mixins;
 
 import dev.bebomny.beaver.beaverutils.client.BeaverUtilsClient;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,6 +12,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ClientPlayerInteractionManager.class)
 public class ClientPlayerInteractionManagerMixin {
 
+
+    @Inject(at = {@At(value = "INVOKE",
+            target = "Lnet/minecraft/client/network/ClientPlayerEntity;getId()I",
+            ordinal = 0)},
+            method = {"updateBlockBreakingProgress(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)Z"})
+    private void onPlayerDamageBlock(BlockPos pos, Direction direction, CallbackInfoReturnable<Boolean> cir) {
+        BeaverUtilsClient.getInstance().autoTool.onBlockBreakingEvent(pos, direction);
+    }
 
     @Inject(method = "getReachDistance()F", at = @At("HEAD"), cancellable = true)
     public void onGetReachDistance(CallbackInfoReturnable<Float> cir) {
