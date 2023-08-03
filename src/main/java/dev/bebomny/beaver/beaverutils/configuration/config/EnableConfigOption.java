b240759a6d7cb6d73ebe25b8974fee462bdf6406
Domniakk <lukaskrzyszewski@gmail.com>
2023-08-03 20:32:12 +0200

    Initial remap, fixes and changes next commit

--- src/main/java/dev/bebomny/beaver/beaverutils/configuration/config/EnableConfigOption.java
+++
@@ -1,9 +0,0 @@
-package dev.bebomny.beaver.beaverutils.configuration.config;
-
-import com.google.gson.annotations.Expose;
-
-public abstract class EnableConfigOption {
-
-    @Expose
-    public boolean enabled = false;
-}
--- src/main/java/dev/bebomny/beaver/beaverutils/configuration/config/NoFallDmgConfig.java
+++
@@ -1,8 +0,0 @@
-package dev.bebomny.beaver.beaverutils.configuration.config;
-
-public class NoFallDmgConfig extends EnableConfigOption{
-
-    public NoFallDmgConfig() {
-
-    }
-}
